97d980fe0088caaf1355353faec7f326013214ff
+ <+>
2024-10-21 17:35:24 +0630

    fetch projects and show in home and singleproject component

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="project" @click="showDetail=!showDetail">
+    <h3>{{ project.title }}</h3>
+    <p v-if="showDetail">{{ project.detail }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+  data() {
+    return {
+      showDetail: false
+    }
+  }
+}
+</script>
+
+<style>
+.project {
+  padding: 20px;
+  background-color: #f2f2f2;
+  cursor: pointer;
+  margin: 10px;
+}
+
+h3 {
+  color: indigo;
+}
+
+p {
+  color: #2c3e50;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project"></SingleProject>
+    </div>
   </div>
 </template>
 <script>
+import SingleProject from '@/components/SingleProject.vue';
 export default {
   name: 'Home',
+  components: {
+    SingleProject
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch("http://127.0.0.1:3000/projects")
+      .then((response) => {
+        return response.json();
+      })
+      .then((datas) => {
+        this.projects = datas
+      })
+      .catch((err) => {
+
+      })
+  }
 }
 </script>

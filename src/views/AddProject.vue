6d44ad84b788fb1a2bdeefee19024e8a0ba66af8
+ <+>
2024-10-22 23:35:22 +0630

    add project

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,22 +1,46 @@
 <template>
     <h1>Add Project</h1>
-    <form>
+    <form @submit.prevent="addProject">
         <label>Project Title</label>
-        <input type="text">
+        <input type="text" v-model="title">
         <label>Project Detail</label>
-        <textarea cols="30" rows="10"></textarea>
+        <textarea cols="30" rows="10" v-model="detail"></textarea>
         <button>Add Project</button>
     </form>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            title: "",
+            detail: ""
+        }
+    },
+    methods: {
+        addProject() {
+            fetch("http://localhost:3000/projects", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(
+                    {
+                        title: this.title,
+                        detail: this.detail,
+                        complete: false
+                    }
+                )
+            })
+            .then(() => {
+                this.$router.push("/");
+            });
+        }
+    }
 }
 </script>
 
 <style scoped>
-
 form {
     background: white;
     padding: 20px;

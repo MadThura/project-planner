cc43c987d1681d7f91c57f2eab6db9ad0597253b
+ <+>
2024-10-22 15:11:15 +0630

    dynamic complete project feature both api and local pj

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="project" :class="{complete:project.complete}">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="flexing">
       <div>
         <h3 @click="showDetail = !showDetail">{{ project.title }}</h3>
@@ -7,7 +7,7 @@
       <div>
         <span class="material-icons" @click="deleteProject">delete</span>
         <span class="material-icons">edit</span>
-        <span class="material-icons">done</span>
+        <span class="material-icons" @click="completeProject">done</span>
       </div>
     </div>
     <p v-if="showDetail">{{ project.detail }}</p>
@@ -27,12 +27,33 @@ export default {
     deleteProject() {
       let deleteRoute = this.api + this.project.id;
       fetch(deleteRoute, { method: "DELETE" })
-      .then((response)=> {
-        this.$emit("delete", this.project.id);
+        .then((response) => {
+          this.$emit("delete", this.project.id);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    },
+
+    completeProject() {
+      let updateCompleteRoute = this.api + this.project.id;
+      fetch(updateCompleteRoute, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+          {
+            complete: !this.project.complete
+          }
+        )
+      })
+      .then(() => {
+        this.$emit("complete", this.project.id);
       })
       .catch((err) => {
         console.log(err.message);
-      });
+      })
     }
   }
 }
@@ -74,5 +95,4 @@ span:hover {
 .complete {
   border-left-color: green;
 }
-
 </style>
